b91553eba8a1088449b55c476a113ceaa37fd0aa
Adam Walter <adam.iverson.walter@gmail.com>
2017-10-25 21:01:37 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
  i = 0
  new_collection = []
 
- while i < 0 && yield(collection[i]) == true
+ while i < 0 && yield(collection[i])
    new_collection << collection[i]
    i += 1
  end
